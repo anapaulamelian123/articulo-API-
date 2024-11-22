d7f41495b3d1c194e1a7094a7d13a400c551575b
anapaulamelian123 <anapaulamelian@gmail.com>
2024-11-21 21:05:37 -0300

    Add files via upload

---
+++ Repositorio.cs
@@ -0,0 +1,68 @@
+﻿using ArticulosAPI.Data;
+using ArticulosAPI.Dto;
+using ArticulosAPI.Modelos;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticulosAPI.Repositorio
+{
+    public class Repositorio : iRepositorio
+
+    {
+        private readonly ApplicationDbContext _context;
+        private IMapper _mapper;    
+        public Repositorio(ApplicationDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;   
+            _context = context; 
+        }
+
+        public async Task<ArticuloDto> CrearOActualizar (ArticuloDto articulo, int id = 0)
+        {
+            Articulo articulos = _mapper.Map<ArticuloDto, Articulo>(articulo);
+            if(id > 0)
+            {
+                articulos.Id = id;
+                _context.Articulos.Update(articulos);
+            }
+            else
+            {
+                await _context.Articulos.AddAsync(articulos);
+            }
+            await _context.SaveChangesAsync();
+            return _mapper.Map<Articulo , ArticuloDto>(articulos);
+        }
+
+        public async Task<bool> EliminarArticulo(int id)
+        {
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo == null)
+            {
+                return false;
+            }
+
+            _context.Articulos.Remove(articulo);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<ArticuloDto>> GetArticulo()
+        {
+
+            List<Articulo> articulos = await _context.Articulos.ToListAsync();
+            return _mapper.Map<List<ArticuloDto>>(articulos);
+        }
+        public async Task<ArticuloDto> GetArticulo(int id)
+        {
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo == null)
+            {
+                return null;
+            }
+            return _mapper.Map<ArticuloDto>(articulo);
+        }
+
+    }
+}
---
+++ iRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ArticulosAPI.Dto;
+
+namespace ArticulosAPI.Repositorio
+{
+    public interface iRepositorio
+    {
+     
+            Task<List<ArticuloDto>> GetArticulo();
+            Task<ArticuloDto> GetArticulo(int id);
+            Task<ArticuloDto> CrearOActualizar(ArticuloDto Articulo, int id = 0);
+            Task<bool> EliminarArticulo (int id);
+        }
+}
